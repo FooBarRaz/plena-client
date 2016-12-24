6bc62ce7d84ad644407e17bc2e76040a3fc15acc
Raz <mehraz.hakim@gmail.com>
2016-12-23 20:44:27 -0500

    added test test to tests

---
+++ plena/src/pages/about/about.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, async } from '@angular/core/testing';
+import { TestUtils }               from '../../test';
+import { AboutPage }               from './about';
+
+let fixture: ComponentFixture<AboutPage> = null;
+let instance: any = null;
+
+describe('Pages: About', () => {
+
+  beforeEach(async(() => TestUtils.beforeEachCompiler([AboutPage]).then(compiled => {
+    fixture = compiled.fixture;
+    instance = compiled.instance;
+  })));
+
+  it('should create the hello ionic page', async(() => {
+    expect(instance).toBeTruthy();
+  }));
+
+});
